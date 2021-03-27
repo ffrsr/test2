8ef6c1cce692241827110c1c0205c179dc5e76af
ffrsr <rossarin.869@mail.kmutt.ac.th>
2021-03-28 05:03:58 +0700

    no message

---
+++ สอบปฏิบัติข้อ2/สอบปฏิบัติข้อ2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace สอบปฏิบัติข้อ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int x = a, y = b;
+
+            if ( x == y)
+            {
+                Console.WriteLine(x);
+            }
+            else
+            {
+                while (x != y)
+                {
+                    if (x < y)
+                    {
+                        x = x + a;
+                    }
+                    else
+                    {
+                        y = y + b;
+                    }                   
+                }
+                Console.WriteLine(x);
+            }                      
+            Console.ReadLine();
+
+            
+
+        }
+    }
+}
